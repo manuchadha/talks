218ca523a0259f024f0bfcb56ba4b92766627b94
manuchadha <manu.chadha87@gmail.com>
2017-01-18 00:17:53 +0530

    Update gopher2.go

--- concurrency/gopher2.go
+++ concurrency/gopher2.go
@@ -2,30 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
-
-//GOPHER OMIT
-func gopher(msg string, c chan string) {
-	for i := 0; ; i++ { //notice the loop keeps going forever
-		c <- fmt.Sprintf("%s %d", msg, i)                            // sending a string value on c chan string
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond) //simulate some work.sleep interval random!
-	}
-}
-
-//ENDGOPHER OMIT
-
 //MAIN OMIT
 func main() {
-	c := make(chan string)     //declared and initialzied a channel of type string
-	go gopher("I am cool!", c) //launch the goroutine,pass channel to communicate back on
-	// we will listen to only 5 channel messages from gopher since we are busy folk.
-	for i := 0; i < 5; i++ {
-		fmt.Printf("Gopher says: %q\n", <-c) // receiving a string value from the channel
-	}
-	//ok enough! bye!
-	fmt.Println("Main: You talk too much. Bye!")
+	c := make(chan int)  //declared and initialzied a channel of type int
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i   // sending a int value on c chan int
+		}
+	}()
+	go func() {
+		for {
+			fmt.Println(<-c)  // receiving a int value from the channel
+		}
+	}()
+	time.Sleep(time.Second)
 }
-
 //ENDMAIN OMIT
