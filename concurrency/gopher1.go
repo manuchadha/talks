f8a50eed946a74064e03c1910386990b26cb9012
manuchadha <manu.chadha87@gmail.com>
2017-01-18 00:05:17 +0530

    Update gopher1.go

--- concurrency/gopher1.go
+++ concurrency/gopher1.go
@@ -10,19 +10,19 @@ func main() {
 	go foo()
 	go bar()
 	fmt.Println("Hello Gopher!")
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
 	fmt.Println("Bye Gopher!")
 }
 
 //END OMIT
 func foo() {
-	for i := 0; i < 45; i++ {
+	for i := 0; i < 5; i++ {
 		fmt.Println("Foo:", i)
 	}
 }
 
 func bar() {
-	for i := 0; i < 45; i++ {
+	for i := 0; i < 5; i++ {
 		fmt.Println("Bar:", i)
 	}
 }
