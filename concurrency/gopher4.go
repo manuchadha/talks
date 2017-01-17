e5524fb60ceda151133698471777f1c6b2dbcdda
manuchadha <manu.chadha87@gmail.com>
2017-01-18 00:39:48 +0530

    Update gopher4.go

--- concurrency/gopher4.go
+++ concurrency/gopher4.go
@@ -1,35 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-//GOPHER OMIT
-func gopher(msg string) <-chan string {
-	c := make(chan string)
-	go func() {
-		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%s %d", msg, i)
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-		}
-	}()
-
-	return c
-}
-
-//ENDGOPHER OMIT
+import "fmt"
 
 //MAIN OMIT
 func main() {
-	bat := gopher("Bat")
-	robin := gopher("Robin")
-	for i := 0; i < 5; i++ {
-		fmt.Printf("I am Gopher%v\n", <-bat)
-		fmt.Printf("I am Gopher%v\n", <-robin)
+	c := incrementor()
+	cSum := puller(c)
+	for n := range cSum {
+		fmt.Println(n)
 	}
-	fmt.Println("You both talk too much. Bye!")
+}
+//ENDMAIN OMIT
+
+//GOPHER OMIT
+func incrementor() chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			out <- i
+		}
+		close(out)
+	}()
+	return out
 }
 
-//ENDMAIN OMIT
+func puller(c chan int) chan int {
+	out := make(chan int)
+	go func() {
+		var sum int
+		for n := range c {
+			sum += n
+		}
+		out <- sum
+		close(out)
+	}()
+	return out
+}
+//ENDGOPHER OMIT
